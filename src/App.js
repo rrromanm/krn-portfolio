eced56c809bc27c5574747f97ba60e5d2a96f669
Romans Mihalonoks <roma.mihalonok@gmail.com>
2023-11-14 18:48:35 +0100

    background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/navbar';
+import Navbar from './Components/navbar/navbar';
+import Home from './Components/home/home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Components/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './home.css'
+
+function home() {
+  return (
+    
+    <div className="bg-image">
+        <h1>KARINA MIHALONOKA</h1>
+    </div>
+  );
+}
+
+export default home;
