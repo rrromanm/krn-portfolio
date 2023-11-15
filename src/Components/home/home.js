de653a55e6c8559217f984177cbdf755300715d0
Romans Mihalonoks <roma.mihalonok@gmail.com>
2023-11-15 14:00:12 +0100

    home section almost done

--- src/Components/home/home.js
+++ src/Components/home/home.js
@@ -4,10 +4,27 @@ import './home.css'
 
 function home() {
   return (
-    
-    <div className="bg-image">
-        <h1>KARINA MIHALONOKA</h1>
-    </div>
+      <div className="col-md-6" id="text">
+        <h3 className="mont-light" id="hi">Hi,</h3>
+        <h1 className="mont-medium" id="heading">I'm Karina Mihalonoka</h1>
+        <h5 className="mont-light" id="description">
+          I am professional photographer based on Benidorm, <br></br>
+          creating dreamscapes with black, white and shades inbetween.
+        </h5>
+
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <button type="button" className='btn btn-light align-self-end'><a href="#gallery">Gallery</a></button>
+            </div>
+            <div className='col'>
+              <button type="button" className='btn btn-light'><a href="#contact">Contact me</a></button>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
   );
 }
 
