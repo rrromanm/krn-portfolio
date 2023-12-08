56c8976df91257ae458bc00d21b008d4a589a4ae
Romans Mihalonoks <roma.mihalonok@gmail.com>
2023-12-08 21:02:44 +0100

    Contact form component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Navbar from './Components/navbar/navbar';
 import Home from './Components/home/home';
 import About from './Components/about/about';
 import Gallery from './Components/gallery/gallery';
+import Contact from './Components/contact/contact';
 import Footer from './Components/footer/footer';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Home/>
       <About/>
       <Gallery/>
+      <Contact/>
       <Footer/>
     </div>
   );
---
+++ src/Components/contact/contact.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faPhone, faMessage} from '@fortawesome/free-solid-svg-icons';
+
+const Contact = () => {
+    
+  return (
+    <div className='contact'>
+      <div className='container pt-5'>
+        <h1 className='py-5 text-white'>Contact Me</h1>
+        
+        <form className='col-md-6 mx-auto '>
+            <div className='mb-3'>
+                <FontAwesomeIcon icon={faUser} className='icon text-white fa-lg d-flex'/>
+                <input type='text' className='input-field form-control form-control-lg text-white' id='floatingName' placeholder='Name' required/>
+            </div>
+            
+            <div className='mb-3'>
+                <FontAwesomeIcon icon={faEnvelope} className='icon text-white fa-lg d-flex'/>
+                <input type='email' className='input-field form-control form-control-lg text-white' id='floatingName' placeholder='Email' required/>
+            </div>
+
+            <div className='mb-3'>
+                <FontAwesomeIcon icon={faPhone} className='icon text-white fa-lg d-flex'/>
+                <input type='tel' className='input-field form-control form-control-lg text-white' id='floatingName' placeholder='Phone' required/>
+            </div>
+
+            <div className='mb-3'>
+                <FontAwesomeIcon icon={faMessage} className='icon text-white fa-lg d-flex'/>
+                <textarea className='input-field form-control form-control-lg text-white' id='textarea' rows={3} placeholder='Message'></textarea>
+            </div>
+
+            <button type='submit' className='btn btn-transparent border text-white d-flex'>
+                SUBMIT
+            </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
