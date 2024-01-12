16f3ecb38aaabdfdb51f1898db7beffb70d65b8b
Romans Mihalonoks <roma.mihalonok@gmail.com>
2024-01-12 16:43:03 +0100

    responsiveness

--- src/Components/b-and-w-gallery/black-and-white.js
+++ src/Components/b-and-w-gallery/black-and-white.js
@@ -6,24 +6,28 @@ const BlackAndWhite = () => {
     [2, 7, 12],
     [3, 8, 13],
     [4, 9, 14],
-    [5, 10],
   ];
 
   return (
+    <div className="container-fluid">
+      <h1 className='pt-5'>Black and White Photography</h1>
     <div className="row g-2 p-4">
       {images.map((imageGroup, index) => (
-        <div className="col mb-4" key={index}>
+        <div className="col-md-3 col-sm-4 col-12" key={index}>
           {imageGroup.map((imageNumber) => (
-            <img
-              key={imageNumber}
-              src={process.env.PUBLIC_URL + `/assets/BandW/${imageNumber}.jpeg`}
-              className="w-100 shadow-1-strong rounded mb-2"
-              alt={`Image ${imageNumber}`}
-            />
+            <div key={imageNumber} className="mb-2">
+              <img
+                src={process.env.PUBLIC_URL + `/assets/BandW/${imageNumber}.jpeg`}
+                className="w-100 shadow-1-strong rounded"
+                alt={`${imageNumber}`}
+              />
+            </div>
           ))}
         </div>
       ))}
     </div>
+    </div>
+    
   );
 };
 
