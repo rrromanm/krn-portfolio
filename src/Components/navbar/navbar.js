82bcdc1113498c1108124870765ee7537848d1ab
Romans Mihalonoks <roma.mihalonok@gmail.com>
2024-01-23 17:05:17 +0100

    navabar dropdown links

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function myNavbar() {
   return (
@@ -13,12 +14,24 @@ function myNavbar() {
             <Nav.Link href="#link">About me</Nav.Link>
 
             <NavDropdown title="Gallery" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Black and White</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Animals</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">City</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Landscape</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Portraits</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Cars</NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/black-and-white">Black and White</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/animals">Animals</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/city">City</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/landscape">Landscape</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/portraits">Portraits</Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link to="/cars">Cars</Link>
+              </NavDropdown.Item>
             </NavDropdown>
 
             <Nav.Link href="#link">Services</Nav.Link>
